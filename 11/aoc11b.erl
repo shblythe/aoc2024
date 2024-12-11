1824dd0e6a4e4a6d22e5d352153c73f36d84a9f6
Stephen Blythe <stephen.blythe@arm.com>
2024-12-11 07:46:54 +0000

    Day 11, part 2
    Change-Id: I043d723e2c14a22a8c30bc900633d1304ee62fac

--- 11/aoc11b.erl
+++ 11/aoc11b.erl
@@ -5,21 +5,35 @@
 %%% Blinks a single stone N times and returns the total number of stones
 blink(0, _) -> 1;
 blink(N, Stone) ->
+    Cached = get({N, Stone}),
     if
-        Stone == 0 -> blink(N-1, 1);
+        Cached /= undefined ->
+            io:format("Cache hit: ~w ~w~n", [N, Stone]),
+            Cached;
         true ->
-            Str = integer_to_list(Stone),
-            Len = length(Str),
-            if
-                (Len rem 2) == 0 ->
-                    blink(N-1, list_to_integer(string:substr(Str,1,Len div 2))) +
-                    blink(N-1, list_to_integer(string:substr(Str,Len div 2+1)));
+            Result = if
+                Stone == 0 -> blink(N-1, 1);
                 true ->
-                    blink(N-1, Stone * 2024)
-            end
+                    Str = integer_to_list(Stone),
+                    Len = length(Str),
+                    if
+                        (Len rem 2) == 0 ->
+                            blink(N-1, list_to_integer(string:substr(Str,1,Len div 2))) +
+                            blink(N-1, list_to_integer(string:substr(Str,Len div 2+1)));
+                        true ->
+                            blink(N-1, Stone * 2024)
+                    end
+            end,
+            put({N, Stone}, Result),
+            Result
     end.
+
+blinks(N, Stones) ->
+    lists:foldl(fun (Stone, Sum) -> Sum + blink(N, Stone) end, 0, Stones).
 
 do(File) ->
     code:add_path(".."),
-    Stones=helpers:read_file_of_ints(File).
+    erase(),    % clear the cache
+    Stones=helpers:read_file_of_ints(File),
+    blinks(75, Stones).
 
